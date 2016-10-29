0b9cd5dfd2c694d7484840afaba61e5b5c2ae4e0
anabordeianu <anamariabordeianu9@gmail.com>
2016-10-29 18:30:42 +0300

    js file

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Marcel are mere")
+             var button=document.getElementById("search");
+
+             button.addEventListener("click", function() {
+                 alert("You pressed!");
+             })
